6aa44f31b10789697deffb0c6e017f95327376ec
TheResearchLab <61911580+TheResearchLab@users.noreply.github.com>
2022-09-30 20:30:42 -0400

    Add files via upload

--- BeautifulSoup_SearchExtract.py
+++ BeautifulSoup_SearchExtract.py
@@ -5,3 +5,35 @@ Created on Wed Sep 28 19:15:46 2022
 @author: Aaron
 """
 
+from bs4 import BeautifulSoup
+import lxml
+import requests
+import re
+
+
+url = "https://webscraper.io/test-sites/e-commerce/allinone/phones/touch"
+
+page = requests.get(url)
+soup = BeautifulSoup(page.text,'lxml')
+
+#find
+soup.find('div',{'class':'container test-site'})
+soup.find('h4',{'class':'pull-right price'})
+
+#find all
+soup.find_all('h4',{'class':'pull-right price'})[1]
+
+soup.find_all('p',class_='pull-right')
+
+#Find specific tags
+soup.find_all(['h4','p'])
+
+#Find all lines with id
+soup.find_all(id=True)
+
+#String find
+soup.find_all(string='Iphone')
+
+
+#string find using re
+soup.find_all(string=re.compile('Iph'))
